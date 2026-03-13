3cc71bea696e6b33211aec546b94627e3a1779ec
Matonice <omotoso.abdulmatin4@gmail.com>
2026-03-13 05:51:15 +0100

    add script for evaluating

--- architecture/gpt.py
+++ architecture/gpt.py
@@ -4,6 +4,17 @@ from architecture.transformer_block import TransformerBlock
 from architecture.layer_norm import LayerNorm
 
 
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,   
+    "context_length": 256, 
+    "emb_dim": 768,        
+    "n_heads": 12,         
+    "n_layers": 12,        
+    "drop_rate": 0.1,      
+    "qkv_bias": False      
+}
+
+
 class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
---
+++ evaluation/eval.py
@@ -0,0 +1,87 @@
+import torch
+from architecture.gpt import GPTModel, GPT_CONFIG_124M
+from data.data_preparation import create_dataloader_v1
+from data.data_preparation import text_data
+
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.eval();  # Disable dropout during inference
+
+# Train/validation ratio
+train_ratio = 0.90
+split_idx = int(train_ratio * len(text_data))
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+train_loader = create_dataloader_v1(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+
+val_loader = create_dataloader_v1(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    return loss
+
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce the number of batches to match the total number of batches in the data loader
+        # if num_batches exceeds the number of batches in the data loader
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Note:
+# Uncommenting the following lines will allow the code to run on Apple Silicon chips, if applicable,
+# which is approximately 2x faster than on an Apple CPU (as measured on an M3 MacBook Air).
+# However, the resulting loss values may be slightly different.
+
+#if torch.cuda.is_available():
+#    device = torch.device("cuda")
+#elif torch.backends.mps.is_available():
+#    device = torch.device("mps")
+#else:
+#    device = torch.device("cpu")
+#
+# print(f"Using {device} device.")
+
+
+model.to(device) 
+
+with torch.no_grad(): 
+    train_loss = calc_loss_loader(train_loader, model, device)
+    val_loss = calc_loss_loader(val_loader, model, device)
+
+print("Training loss:", train_loss)
+print("Validation loss:", val_loss)
